44d5d20717653eb51746e8930edb0a5ae03ee65d
vzdzt <aproca24@gmail.com>
2025-10-20 05:50:06 -0400

    Changed bottom to -95px as user requested

--- experimental.js
+++ experimental.js
@@ -170,7 +170,7 @@ function createCollapsibleControlPanel() {
     tab.innerHTML = '⚙️';
     tab.style.cssText = `
         position: absolute;
-        bottom: -105px; /* Trying -105px as user requested */
+        bottom: -95px; /* Trying -95px as user requested */
         right: 70px; /* Horizontal position */
         width: 32px;
         height: 32px;
