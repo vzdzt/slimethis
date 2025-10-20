571559c98f1696e1cb80559270d94e90fb436232
vzdzt <aproca24@gmail.com>
2025-10-20 04:13:40 -0400

    Fixed positioning logic by using left with viewport calculation instead of right

--- experimental.js
+++ experimental.js
@@ -168,7 +168,7 @@ function createCollapsibleControlPanel() {
     tab.style.cssText = `
         position: fixed;
         bottom: 20px;
-        right: 50px; /* Increased to bring wheel more into view */
+        left: calc(100vw - 52px); /* Use viewport width minus offset */
         width: 32px;
         height: 32px;
         background: rgba(0, 0, 0, 0.9);
